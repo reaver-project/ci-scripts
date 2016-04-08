1044751234152d903913b4bfea440e855ab507a6
Michał 'Griwes' Dominiak <griwes@griwes.info>
2016-04-08 13:09:28 +0200

    Initial attempt at Github CI status; only 'pending' so far.

---
+++ github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import slumber
+import requests
+
+
+class Session:
+    def __init__(self, url, user, token):
+        self.api.session = requests.session()
+        self.api_session.headers['Accept'] = 'application/vnd.github.v3+json'
+
+        self.auth = requests.auth.HTTPBasicAuth(user, token)
+
+        self.api = slumber.API(url, session = self.api_session, auth = self.auth)
+
+    def set_status(self, owner, repo, sha, params):
+        self.api.repos(owner)(repo).statuses(sha).post(params)
